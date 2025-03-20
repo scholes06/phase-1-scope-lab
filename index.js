5c51d1f8865f8595cb332b9c861cf48a85a4023c
Agostino <agostino.omare@student.moringaschool.com>
2025-03-21 00:11:05 +0300

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}    
+
+const leastFavouriteCustomer = "someone";
+   
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = "someone else";
+}
